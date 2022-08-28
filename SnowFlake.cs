89ffa304c8994d084312df38b932a2c04bd9a983
Nebware <69011582+Nebware@users.noreply.github.com>
2022-08-28 14:58:58 +0300

    Add files via upload

---
+++ SnowFlake.cs
@@ -0,0 +1,14 @@
+﻿namespace Discord
+{
+    public class SnowFlake
+    {
+        public static long CreateDiscordSnowflake()
+        {
+            var TimeStamp = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            var DiscordE = 1420070400000;
+            var Difference = (TimeStamp - DiscordE) % 4398046511104;
+            var SnowFlake = Difference << 5 << 5 << 12;
+            return SnowFlake;
+        }
+    }
+}
